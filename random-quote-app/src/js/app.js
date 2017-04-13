318fbdc0b961bb681aa0fee3a2726203b8a619f0
Zerk <axzerk@gmail.com>
2017-04-13 23:52:33 +0300

    added editorconfig

--- random-quote-app/src/js/app.js
+++ random-quote-app/src/js/app.js
@@ -1,31 +1,30 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  let getQuoteBtn = $('.quote-btn'),
+    tweetBtn = $('.tweet-btn'),
+    quoteTextField = $('.quote-text > span'),
+    quoteAuthorField = $('.quote-author');
 
-    var getQuoteBtn = $(".quote-btn"),
-        tweetBtn = $(".tweet-btn"),
-        quoteTextField = $(".quote-text > span"),
-        quoteAuthorField = $(".quote-author");
+  getQuote(quoteTextField, quoteAuthorField);
 
+  getQuoteBtn.on('click', function () {
     getQuote(quoteTextField, quoteAuthorField);
+  });
 
-    getQuoteBtn.on("click", function() {
-        getQuote(quoteTextField, quoteAuthorField);
+  function getQuote(textField, authorField) {
+    $.ajax({
+      headers: {
+        "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=",
+      success: function (response) {
+        let data = JSON.parse(response);
+        textField.html(data.quote);
+        authorField.html(`&mdash; ${data.author}`);
+
+        tweetBtn.attr('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(` ${data.quote} ${data.author}`)}`);
+      }
     });
-
-    function getQuote(textField, authorField) {
-        $.ajax({
-            headers: {
-                "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
-                Accept: "application/json",
-                "Content-Type": "application/x-www-form-urlencoded"
-            },
-            url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=",
-            success: function(response) {
-                var data = JSON.parse(response);
-                textField.html(data.quote);
-                authorField.html(`&mdash; ${data.author}`);
-
-                tweetBtn.attr('href', `https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=${encodeURIComponent(` ${data.quote} ${data.author}`)}`);
-            }
-        });
-    }
+  }
 });
